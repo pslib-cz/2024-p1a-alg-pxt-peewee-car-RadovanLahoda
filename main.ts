5ea852dd04efe7b1da7c905b9a19b9db0269c599
bomboklada <rada.lahoda08@gmail.com>
2025-05-09 12:45:41 +0200

    Setup

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+basic.forever(function () {
+    if (RecievedNumber === 1) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
+    }
+})
+let RecievedNumber = radio.receiveNumber()
