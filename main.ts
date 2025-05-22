a6e942170dbb0455f7916c9258ad9ff94acb13c3
bomboklada <radovan.lahoda.024@pslib.cz>
2025-05-22 21:27:04 +0200

    Zavody bez komentaru main.ts

--- main.ts
+++ main.ts
@@ -17,14 +17,7 @@ let kousek3 = 0
 let oves = 0
 let uhel = 0
 let uzel = 0
-//if (trpaslik === false) {
-//    radio.onReceivedString(function (ReceivedString) {
-//        let kousek = ReceivedString.split(',')
-//        kousek3 = parseInt(kousek[2])
-//    })
-//}
 basic.forever(function () {
-    //basic.showNumber(radio.receivedPacket(RadioPacketProperty.SerialNumber))
     radio.onReceivedString(function (ReceivedString) {
         let dilek = ReceivedString.split(',')
         let kousek3 = parseInt(dilek[0])
@@ -73,51 +66,3 @@ basic.forever(function () {
         }
     }) 
 })
-    //if (ludvik14 == -207736456){
-    //    if (true) {
-    //        let x = kocka / 5
-    //        if (x >= 255) {
-    //            x = 250
-    //        }
-    //        if (x <= -255) {
-    //            x = -250
-    //        }
-    //        if (x >= -25 && x <= 25) {
-    //            x = 0
-    //        }
-    //        PCAmotor.MotorRun(PCAmotor.Motors.M1, (x - y) * 2)
-    //    }
-    //    if (true) {
-    //        let y = cocka / 5
-    //        if (y >= 255) {
-    //            y = 250
-    //        }
-    //        if (y <= -255) {
-    //            y = -250
-    //        }
-    //        if (y >= -25 && y <= 25) {
-    //            y = 0
-    //        }
-    //        PCAmotor.MotorRun(PCAmotor.Motors.M4, (y - x / 2) * 2)
-    //        o += 5
-    //        p += 5
-    //        basic.pause(20)
-    //        pasek.showRainbow(o, p)
-    //        pasek.show()
-    //    }
-    //    if (o >= 350) {
-    //        o = -0
-    //    }
-    //    if (p >= 350) {
-    //        p = -0
-    //    }
-    //} else if(pokus == false){
-    //    basic.showLeds(`
-    //    .....
-    //    .....
-    //    .....
-    //    .....
-    //    .....
-    //    `)
-    //    pokus = true
-    //}
